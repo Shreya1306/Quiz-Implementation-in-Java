d32e3eab0ddbc37cf6bf4bc7ffd4ce4c2ac02d99
Shreya1306 <arorashreya1306@gmail.com>
2016-09-22 22:09:41 +0530

    Add files via upload

---
+++ MainClassQuiz.java
@@ -0,0 +1,20 @@
+package quiz;
+
+
+	class MainClassQuiz {
+
+	
+	public static void main(String[] args) {
+
+		  Start_up srt=new Start_up();
+		//Admin_Login adlog=new Admin_Login();
+		//Admin_welcome adwel=new Admin_welcome();
+		//Change_password chpass=new Change_password();
+		//Add_Question addq=new Add_Question();
+		//Edit_Remove_question erqu=new Edit_Remove_question();
+		//Show_Questions sq=new Show_Questions();
+		//Student_record stdrec=new Student_record();
+		
+	}
+
+}
