7c712e830d93517255bd8c593a21e3f40b76a48a
Shreya1306 <arorashreya1306@gmail.com>
2016-09-22 22:08:48 +0530

    Add files via upload

---
+++ list.java
@@ -0,0 +1,94 @@
+package quiz;
+
+public class list {
+	
+	String name,id,date,marks,cat,level,ques_id,ques,op_A,op_B,op_C,op_D,anss,feed;
+	
+	
+
+
+	public String getFeed() {
+		return feed;
+	}
+	public void setFeed(String feed) {
+		this.feed = feed;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getMarks() {
+		return marks;
+	}
+	public void setMarks(String marks) {
+		this.marks = marks;
+	}
+	public String getCat() {
+		return cat;
+	}
+	public void setCat(String cat) {
+		this.cat = cat;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getQues_id() {
+		return ques_id;
+	}
+	public void setQues_id(String quesId) {
+		ques_id = quesId;
+	}
+	public String getQues() {
+		return ques;
+	}
+	public void setQues(String ques) {
+		this.ques = ques;
+	}
+	public String getOp_A() {
+		return op_A;
+	}
+	public void setOp_A(String opA) {
+		op_A = opA;
+	}
+	public String getOp_B() {
+		return op_B;
+	}
+	public void setOp_B(String opB) {
+		op_B = opB;
+	}
+	public String getOp_C() {
+		return op_C;
+	}
+	public void setOp_C(String opC) {
+		op_C = opC;
+	}
+	public String getOp_D() {
+		return op_D;
+	}
+	public void setOp_D(String opD) {
+		op_D = opD;
+	}
+	public String getAnss() {
+		return anss;
+	}
+	public void setAnss(String anss) {
+		this.anss = anss;
+	}
+}
