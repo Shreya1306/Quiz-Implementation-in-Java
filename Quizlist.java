7f4d1467bf703d85ad13036c05ef5d8be96e7245
Shreya1306 <arorashreya1306@gmail.com>
2016-09-22 22:12:32 +0530

    Add files via upload

---
+++ Quizlist.java
@@ -0,0 +1,136 @@
+package quiz;
+
+public class Quizlist {
+	String qid,ques,op_A,op_B,op_C,OP_D,ans,name,dob,cat,lvl,mks,ques_att,inc_ques,dt,id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCat() {
+		return cat;
+	}
+
+	public void setCat(String cat) {
+		this.cat = cat;
+	}
+
+	public String getLvl() {
+		return lvl;
+	}
+
+	public void setLvl(String lvl) {
+		this.lvl = lvl;
+	}
+
+	public String getMks() {
+		return mks;
+	}
+
+	public void setMks(String mks) {
+		this.mks = mks;
+	}
+
+	public String getQues_att() {
+		return ques_att;
+	}
+
+	public void setQues_att(String quesAtt) {
+		ques_att = quesAtt;
+	}
+
+	public String getInc_ques() {
+		return inc_ques;
+	}
+
+	public void setInc_ques(String incQues) {
+		inc_ques = incQues;
+	}
+
+	public String getDt() {
+		return dt;
+	}
+
+	public void setDt(String dt) {
+		this.dt = dt;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getQid() {
+		return qid;
+	}
+
+	public void setQid(String qid) {
+		this.qid = qid;
+	}
+
+	public String getQues() {
+		return ques;
+	}
+
+	public void setQues(String ques) {
+		this.ques = ques;
+	}
+
+	public String getOp_A() {
+		return op_A;
+	}
+
+	public void setOp_A(String opA) {
+		op_A = opA;
+	}
+
+	public String getOp_B() {
+		return op_B;
+	}
+
+	public void setOp_B(String opB) {
+		op_B = opB;
+	}
+
+	public String getOp_C() {
+		return op_C;
+	}
+
+	public void setOp_C(String opC) {
+		op_C = opC;
+	}
+
+	public String getOP_D() {
+		return OP_D;
+	}
+
+	public void setOP_D(String oPD) {
+		OP_D = oPD;
+	}
+
+	public String getAns() {
+		return ans;
+	}
+
+	public void setAns(String ans) {
+		this.ans = ans;
+	}
+	
+	
+
+}
